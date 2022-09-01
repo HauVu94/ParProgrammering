c9a6ca3521607a50e379fe476ea62808fcf11cc8
hau_v <hau_vu94@hotmail.com>
2022-09-01 10:51:19 +0200

    sixth commit

--- parProgrammering.java
+++ parProgrammering.java
@@ -11,13 +11,19 @@ public class parProgrammering {
       void program(){
         int regularNumber = 1;
         assertTrue(formatToRomanNumber(regularNumber));
-
-
     }
     boolean formatToRomanNumber(int formatNumber){
         return true;
     }
 
+    boolean checkFirstRomanNumber(int firstRomanNumber){
+      return firstRomanNumber == 1;
+    }
+    @Test
+    void toRomanNumber(){
+        int firstNumber = 10;
+       assertFalse(formatToRomanNumber(firstNumber));
+    }
 
 }
 
