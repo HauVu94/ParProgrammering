62da160bfb2bef24cc2625ac2fa5823f0e7cf839
hau_v <hau_vu94@hotmail.com>
2022-09-01 11:56:58 +0200

    🟢Johannes hjelp

--- parProgrammering.java
+++ parProgrammering.java
@@ -7,42 +7,40 @@ public class parProgrammering {
     @Test
       void program() {
         int regularNumber = 1;
-        assertEquals("i",convertNumeralOneToRomanValue(regularNumber));
-    }
-
-    String convertNumeralOneToRomanValue(int numberOne){
-        String romanNumberOne = "i";
-        if(numberOne == 1){
-            return romanNumberOne;
-        }
-        return null;
+        assertEquals("i", convertNumeralToRomanValue(regularNumber));
     }
 
     @Test
     void ConvertSecondNumber(){
         int regularNumber = 2;
-        assertEquals("ii", convertNumeralTwoToRomanValue(regularNumber));
+        assertEquals("ii", convertNumeralToRomanValue(regularNumber));
     }
 
-    String convertNumeralTwoToRomanValue(int numberTwo){
-        String romanNumberTwo = "ii";
-        if (numberTwo == 2) {
-            return romanNumberTwo;
-        }
-        return null;
-    }
+
     @Test
     void convertThirdNumber(){
-        int regularNumber = 4;
-        assertEquals("iii", convertNumeralThreeToRomanValue(regularNumber));
+        int regularNumber = 3;
+        assertEquals("iii", convertNumeralToRomanValue(regularNumber));
 
     }
-    String convertNumeralThreeToRomanValue(int numberThree){
+    String convertNumeralToRomanValue(int number){
         String romanNumberThree = "iii";
-        if(numberThree == 3){
+        if(number == 3){
             return romanNumberThree;
+        }
+        String romanNumberTwo = "ii";
+        if (number == 2) {
+            return romanNumberTwo;
+        }
+        String romanNumberOne = "i";
+        if(number == 1){
+            return romanNumberOne;
         }
         return null;
     }
+
+
+
+
 }
 
